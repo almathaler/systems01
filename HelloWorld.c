565adb05e154103b89dd3756e36f95adf5ec837d
almathaler <athaler00@stuy.edu>
2019-09-08 17:33:54 -0400

    fixed that %lf didn't show full range of number by doing %0.20lf instead

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,4 +1,7 @@
 int main() {
- printf("Good News Everyone!\n you're hired!");
+ printf("Good News Everyone!\n you're hired!\n");
+ printf("Here is a number: %d and then here is a really long number: %ld\n", 4, 345345345345345);
+ printf("here is a float: %0.2f and then here is a double: %0.20lf (I like that it's called long float)\n", 5.68, 5.68686839683934);
+ printf("here is a char: %c and I guess here is a string but I don't know what will happen: %s\n", 'C', "'literal'");
  return 0;
 }
